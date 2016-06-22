5611c1a2b596b6a103fece8ef0ae94ac20a394d3
Fahad Alodan <FahadAlodan@Fahads-MacBook-Air.local>
2016-06-21 22:58:33 -0500

    fixes

--- comments.php
+++ comments.php
@@ -19,11 +19,9 @@
 	$user_id = $row["UID"];
     $time = time();
     
-	echo "$name".$user_id.$content.$PID.$time."\n";
-    
 	$result_insert = mysqli_query($conn, "INSERT INTO comments (post_id, comment, user_id, user_name, time) VALUES ($PID, '$content', '$user_id', '$name', now())");
   //  INSERT INTO `comments`(`post_id`, `comment`, `user_id`, `user_name`, `time`) VALUES ()
-    echo $result_insert;
+   // echo $result_insert;
 	//check if insert was okay
 	if($result_insert){
 
--- database.php
+++ database.php
@@ -13,4 +13,4 @@ function connect_db(){
 	return $conn;
 }	
 
-?>	
+?>
